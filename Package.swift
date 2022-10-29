0bd724a9647125bf7ddbbcee54f1c08b9b1dca62
Alireza Asadi <alireza.asadi.36@gmail.com>
2022-10-29 17:33:26 +0330

    Reorder files and folders. Rename the project.

--- Package.swift
+++ Package.swift
@@ -3,23 +3,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "telegram-vapor-bot",
+    name: "telegram-bot",
     platforms: [
         .macOS(.v10_15),
     ],
     products: [
         .library(
-            name: "TelegramVaporBot",
-            targets: ["TelegramVaporBot"]
+            name: "TelegramBot",
+            targets: ["TelegramBot"]
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.45.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.66.1"),
         .package(url: "https://github.com/nerzh/swift-regular-expression", from: "0.2.4"),
     ],
     targets: [
         .target(
-            name: "TelegramVaporBot",
+            name: "TelegramBot",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "SwiftRegularExpression", package: "swift-regular-expression"),
